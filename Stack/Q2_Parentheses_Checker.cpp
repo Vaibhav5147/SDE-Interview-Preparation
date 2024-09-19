400d03f2ac6e6f8954535593e56f7162b84396c1
vaibhav-raut <vaibhav.dakshana17@gmail.com>
2024-09-19 19:36:26 +0530

    Space optimized approach for parentheses checker

--- Stack/Q2_Parentheses_Checker.cpp
+++ Stack/Q2_Parentheses_Checker.cpp
@@ -6,10 +6,24 @@ using namespace std;
      For example, the function should return 'true' for exp = [()]{}{[()()]()} and 'false' for exp = [(]).
 */
 class Solution
-{
+{  
+     private:
+     bool isMatch(char c1, char c2)
+     {
+        if (c1 == '(' && c2 == ')') return true;
+        if (c1 == '[' && c2 == ']') return true;
+        if (c1 == '{' && c2 == '}') return true;
+            return false;
+     }
+
     public:
     
-    bool isValid(string x)
+    /*
+    Approach : 1 using stack
+    Time complexity : O(N)
+     Space complexity : O(N)
+    */
+    bool isValidUsingStack(string x)
     {
     int n = x.size();
     if(n==1)
@@ -45,8 +59,34 @@ class Solution
             default:;
          }
     }
-return st.empty();
+     return st.empty();
     }
+    /*
+     Approach : 2 , using same string as stack , 
+     Time complexity : O(N)
+     Space complexity : O(1)
+    */
+    bool isValid(string x)
+    {
+        // Your code here
+        int n = x.size();
+        int top = -1;
+        for(int i=0;i<n;i++)
+        {
+            if(top < 0  || !isMatch(x[top],x[i]))
+            {
+                top++;
+                x[top] = x[i];
+            }
+            else
+                top--;
+        }
+        
+        return top == -1;
+        
+    }
+    
+    
 
 };
 
